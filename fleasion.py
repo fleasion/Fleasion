a2b4c69a8276fcdb73519d3155440a856c645c02
Gruber450 <68507886+qrhrqiohj@users.noreply.github.com>
2024-12-24 15:12:11 +0000

    Update fleasion.py

--- fleasion.py
+++ fleasion.py
@@ -1,5 +1,12 @@
-# CLI-DEPRECATED
-# Fleasion-CLI, open sourced cache modifier, intended for Phantom Forces. plz dont abuse D:
-# discord.gg/hXyhKehEZF
+#DEPRECATED
+import ctypes; import webbrowser; import threading; import os; import sys
 
-input("\n\nThis version of fleasion is now deprecated, get the latest version here\n: https://github.com/fleasion/Fleasion/releases/tag/CLI\n\nEnter to exit: ")
+webbrowser.open("https://github.com/fleasion/fleasion/releases/tag/CLI")
+
+ctypes.windll.user32.MessageBoxW(0, 
+    "This version of Fleasion is now deprecated. You've been redirected to download the new one. Select 'Source code (zip)'.", 
+    "Fleasion has been rewritten!", 
+    0x0010 | 0x0001 | 0x00040000)
+
+if os.name == 'nt':  
+    ctypes.windll.user32.ShowWindow(ctypes.windll.kernel32.GetConsoleWindow(), 0)
