592268c0131bd5cc04611aed34765cfd54365f71
Gruber450 <68507886+qrhrqiohj@users.noreply.github.com>
2024-12-24 13:36:49 +0000

    Update fleasion.py

--- fleasion.py
+++ fleasion.py
@@ -2,4 +2,4 @@
 # Fleasion-CLI, open sourced cache modifier, intended for Phantom Forces. plz dont abuse D:
 # discord.gg/hXyhKehEZF
 
-input("This version of fleasion is now deprecated, get the latest version here\n: https://github.com/fleasion/Fleasion/releases/tag/CLI\n\nEnter to exit: ")
+input("\n\nThis version of fleasion is now deprecated, get the latest version here\n: https://github.com/fleasion/Fleasion/releases/tag/CLI\n\nEnter to exit: ")
