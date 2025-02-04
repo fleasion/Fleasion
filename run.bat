5fa5af7da460674df030712ef9e50737b70e3d4e
3tcy <etcy@duck.com>
2025-02-04 21:43:39 +0700

    Maybe this time? PIP detection fix

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
 @echo off
-: v1.3.14
+: v1.3.15
 
 : fleasion by @cro.p, rewrite by @8ar
 : distributed in https://discord.com/invite/fleasion
@@ -13,7 +13,6 @@ setlocal enableDelayedExpansion
 for /f "tokens=2 delims=[]" %%a in ('ver') do set ver=%%a
 for /f "tokens=2,3,4 delims=. " %%a in ("%ver%") do set v=%%a.%%b
 if "%v%"=="10.0" goto setdrive
-if "%v%"=="6.3" goto setdrive
 goto unsupported
 
 : Change partition to the one where the run script is located if it's different
@@ -42,7 +41,7 @@ cls
 set pythonIsInstalled=True
 python --version >> py.txt
 if %errorlevel%==9009 goto py
-: Redundant command repeat to reset it
+: Redundant command repeat so the command actually works LOL
 findstr "3.13" py.txt
 findstr "3.13" py.txt | if %errorlevel%==0 echo Found || set pythonIsInstalled=False
 del py.txt
@@ -58,20 +57,11 @@ echo Installing..
 python-installer.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0 Include_doc=0
 del python-installer.exe
 
-: Fleasion cannot operate without a few pip packages.
 :pip
-echo Checking for pip..
-python -m pip >nul
-if %errorlevel%==1 goto getpip
-if %errorlevel% NEQ 0 goto error
+echo Checking for pip...
+python -m pip >nul 2>&1
+if %errorlevel% != 0 python -m ensurepip --upgrade
 goto fleasion
-
-:getpip
-echo Downloading pip...
-curl -sSL -k -o get-pip.py https://bootstrap.pypa.io/get-pip.py --ssl-no-revoke
-echo Installing..
-py get-pip.py --no-setuptools --no-wheel >nul 2>&1
-del get-pip.py
 
 : Dependency packages moved to storage\auto-update.py
 :                psutil, colorama
@@ -81,26 +71,45 @@ del get-pip.py
 md "%~dp0assets\games" >nul 2>&1
 md "%~dp0assets\community" >nul 2>&1
 md "%~dp0assets\presets" >nul 2>&1
+md "%~dp0assets\custom\storage\cached_files" >nul 2>&1
 md "%~dp0storage" >nul 2>&1
-if not exist "%~dp0storage\LICENSE" if not exist "%~dp0LICENSE" curl -sSL -k -o "%~dp0storage\LICENSE" https://raw.githubusercontent.com/qrhrqiohj/Fleasion-Rewrite-test/refs/heads/main/storage/LICENSE --ssl-no-revoke | move "%~dp0LICENSE" "%~dp0storage\LICENSE"
+if not exist "%~dp0assets\custom\storage\assets.json" (
+    echo { > "%~dp0assets\custom\storage\assets.json"
+    echo    "Explanation": "If you dont want custom hashes to show as unknown or a hash, define them here", >> "%~dp0assets\custom\storage\assets.json"
+    echo    "Name goes Here": "Hash goes Here" >> "%~dp0assets\custom\storage\assets.json"
+    echo } >> "%~dp0assets\custom\storage\assets.json"
+)
+if exist "%~dp0UNZIP BEFORE RUN" del "%~dp0UNZIP BEFORE RUN"
+if not exist "%~dp0storage\LICENSE" (if exist "%~dp0LICENSE" (move /Y "%~dp0LICENSE" "%~dp0storage\LICENSE" >nul 2>&1) else (curl -sSL -k -o "%~dp0storage\LICENSE" https://raw.githubusercontent.com/qrhrqiohj/Fleasion-Backend/refs/heads/main/LICENSE --ssl-no-revoke))
+if not exist "%~dp0storage\README.md" (if exist "%~dp0README.md" (move /Y "%~dp0README.md" "%~dp0storage\README.md" >nul 2>&1) else (curl -sSL -k -o "%~dp0storage\README.md" https://raw.githubusercontent.com/fleasion/Fleasion/refs/heads/rewrite/README.md --ssl-no-revoke))
 if exist "%~dp0storage\autoupdate.py" goto launch
-: echo Downloading updater..
-: curl -sSL -k -o "%~dp0storage\autoupdate.py" %link here% --ssl-no-revoke
+echo Downloading updater..
+curl -sSL -k -o "%~dp0storage\autoupdate.py" https://raw.githubusercontent.com/qrhrqiohj/Fleasion-Backend/refs/heads/main/autoupdate.py --ssl-no-revoke
 
 :launch
 %drive%
 cd %dir%\storage
 cls
-python autoupdate.py
-if %errorlevel% NEQ 0 goto error
-set finished=True
-exit /b
+if exist %~dp0autoupdate.py (
+    python autoupdate.py
+    if %errorlevel% NEQ 0 goto error
+    set finished=True
+)
+if finished == True exit /b
+goto updater_error
 
 :error
-if finished == True exit
-echo x=msgbox("Python failed, isn't added to PATH or the Updater failed to download."+vbCrLf+" "+vbCrLf+"You will be redirected to a discord server where you can report this issue.", vbSystemModal + vbCritical, "Fleasion dependency setup failed") > %temp%\fleasion-error.vbs
+echo x=msgbox("Python failed, is missing or isn't added to PATH."+vbCrLf+" "+vbCrLf+"You will be redirected to a discord server where you can report this issue.", vbSystemModal + vbCritical, "Fleasion dependency setup failed") > %temp%\fleasion-error.vbs
 start /min cscript //nologo %temp%\fleasion-error.vbs
 start "" https://discord.gg/invite/fleasion
+del %temp%\fleasion-error.vbs
+exit /b
+
+:updater_error
+echo x=msgbox("The updater failed to download."+vbCrLf+" "+vbCrLf+"You will be redirected to a website where you can download it manually.", vbSystemModal + vbCritical, "Fleasion dependency setup failed") > %temp%\fleasion-error.vbs
+start /min cscript //nologo %temp%\fleasion-error.vbs
+start "" https://github.com/qrhrqiohj/Fleasion-Backend/blob/main/autoupdate.py
+del %temp%\fleasion-error.vbs
 exit /b
 
 :unsupported
