1dec92fbea720616dd18dad1d24a5d5ddf06d00b
Spencer0187 <sviatoslavsvii@gmail.com>
2026-04-18 21:47:12 -0400

    feat: Bump version to 2.0.1 across project files

--- src/Fleasion/utils/paths.py
+++ src/Fleasion/utils/paths.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 # Application metadata
 APP_NAME = 'Fleasion'
-APP_VERSION = '2.0.0'
+APP_VERSION = '2.0.1'
 APP_AUTHOR = '@8ar__, @dis_spencer, @1_v'
 APP_LOGIC = '@blockce, @0100152000022000, @Yeha., @emk530'
 APP_CONCEPT = '@cro.p'
